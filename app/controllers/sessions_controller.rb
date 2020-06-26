2779d0551c5c48ea00e631748ab12c4bd5a699e9
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-06-26 13:08:17 -0500

    Add user logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out successfully"
+    redirect_to root_path
   end
 end
