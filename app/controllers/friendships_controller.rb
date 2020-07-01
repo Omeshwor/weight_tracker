227497ef577da141297dd67af3ea7de149ae089b
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-07-01 11:56:23 -0500

    Add user's friends show view.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,11 +5,15 @@ class FriendshipsController < ApplicationController
     user_friend = current_user.friendships.build(friend_id: friend.id)
     if user_friend.save
       flash.now[:notice] = "#{friend.full_name} add to friend's list"
-      redirect_to users_path
+      redirect_to friendship_path(current_user)
     else
       flash.now[:danger] = "Something went wrong"
       redirect_to users_path
     end
+  end
+
+  def show
+    @tracked_friends  = current_user.friends.order(:created_at).paginate(page: params[:page], per_page: 10)
   end
 
 end
