6e354dc95905d046d3fbb956e37c0ef61311628b
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-07-01 11:40:26 -0500

    Add show all users and add friend functionality.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,15 @@
-class FriendshipController < ApplicationController
+class FriendshipsController < ApplicationController
+
+  def create
+    friend = User.find(params[:friend])
+    user_friend = current_user.friendships.build(friend_id: friend.id)
+    if user_friend.save
+      flash.now[:notice] = "#{friend.full_name} add to friend's list"
+      redirect_to users_path
+    else
+      flash.now[:danger] = "Something went wrong"
+      redirect_to users_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :user_weights
+  resources :friendships
 
 end
